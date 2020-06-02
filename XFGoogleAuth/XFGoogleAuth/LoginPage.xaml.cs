b6c1f008ca8e422f865d7e49ff89dd7e2c532e09
Shuhey <shuhey.dev@gmail.com>
2020-06-02 15:03:40 +0900

    Add project files.

---
+++ XFGoogleAuth/XFGoogleAuth/LoginPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Plugin.GoogleClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XFGoogleAuth.Services;
+
+namespace XFGoogleAuth
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            //ログイン成功した後はAccessTokenに値が入る.
+            //それを使って初回のみログインを求めるか,毎回求めるかを選択できる
+            //今回は毎回ログインを求めるようにしている
+            if (!string.IsNullOrEmpty(CrossGoogleClient.Current.AccessToken))
+            {
+                //認証済みの場合の処理
+                //例えば他のページに遷移するなど.
+                //App.Current.MainPage=new MainPage();
+
+                CrossGoogleClient.Current.Logout();
+            }
+
+            var loginUser = await CrossGoogleClient.Current.LoginAsync();
+
+            //認証後の処理.
+            //例えば,他のページに遷移するなど.
+            App.Current.MainPage = new MainPage();
+        }
+    }
+}
---
+++ XFGoogleAuth/XFGoogleAuth/Services/GoogleLoginService.cs
@@ -0,0 +1,74 @@
+﻿using Plugin.GoogleClient;
+using Plugin.GoogleClient.Shared;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using XFGoogleAuth.Services;
+
+[assembly: Xamarin.Forms.Dependency(typeof(GoogleLoginService))]
+namespace XFGoogleAuth.Services
+{
+    public class GoogleLoginService
+    {
+        IGoogleClientManager _googleService = CrossGoogleClient.Current;
+
+        public GoogleLoginService()
+        {
+            _googleService.OnLogin += (s, e) =>
+            {
+                switch (e.Status)
+                {
+                    case GoogleActionStatus.Completed:
+#if DEBUG
+                        var googleUserString = JsonSerializer.Serialize(e.Data);
+                        Debug.WriteLine($"Google Logged in succesfully: {googleUserString}");
+#endif
+                        break;
+                }
+            };
+        }
+
+        public async Task LoginAsync()
+        {
+            if (!string.IsNullOrEmpty(_googleService.AccessToken))
+            {
+                _googleService.Logout();
+            }
+            var loginUser = await _googleService.LoginAsync();
+
+
+
+            //            try
+            //            {
+            //                if (!string.IsNullOrEmpty(_googleService.AccessToken))
+            //                {
+            //                    //常にユーザー認証を必要とする
+            //                    _googleService.Logout();
+            //                }
+
+            //                EventHandler<GoogleClientResultEventArgs<GoogleUser>> userLoginDelegate = null;
+            //                userLoginDelegate = async (object sender, GoogleClientResultEventArgs<GoogleUser> e) =>
+            //                      {
+            //                          switch (e.Status)
+            //                          {
+            //                              case GoogleActionStatus.Completed:
+            //#if DEBUG
+            //                                  var googleUserString = JsonSerializer.Serialize(e.Data);
+            //                                  Debug.WriteLine($"Google Logged in succesfully: {googleUserString}");
+            //#endif
+            //                                  var socialLoginData=new NetworkAuthData
+            //                          }
+            //                      };
+            //            }
+            //            catch
+            //            {
+
+            //            }
+        }
+    }
+}
